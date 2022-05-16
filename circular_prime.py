585c2d570b1a9ffb7813a472e435b7adbdf6dbd4
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-05-16 21:36:18 +0530

    n=int(input()) a=0 b=0 for i in range(2,int(n**0.5)+1):     if n%i==0:         break else:     a+=1 rev=0 while n:     d=n%10     n=n//10     rev=(rev*10)+d for i in range(2,int(rev**0.5)+1):     if rev%i==0:         break else:     b+=1 if a==0:     print('not prime') elif a!=0 and b==0:     print('prime but not a circular prime') elif (a!=0 and b!=0):      print('circular prime')

--- circular_prime.py
+++ circular_prime.py
@@ -2,7 +2,7 @@ n=int(input())
 a=0
 b=0
 for i in range(2,int(n**0.5)+1):
-    if(n%i==0):
+    if n%i==0:
         break
 else:
     a+=1
@@ -12,15 +12,14 @@ while n:
     n=n//10
     rev=(rev*10)+d
 for i in range(2,int(rev**0.5)+1):
-    if(rev%i==0):
+    if rev%i==0:
         break
 else:
     b+=1
-if(a==0):
+if a==0:
     print('not prime')
-elif( a!=0 and b==0):
+elif a!=0 and b==0:
     print('prime but not a circular prime')
-elif a!=0 and b!=0:
-    print('circular prime')
-
-
+elif (a!=0 and b!=0):
+     print('circular prime')
+    
