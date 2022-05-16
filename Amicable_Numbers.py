508e14526733c83d55063ddaf08d888da0109136
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-05-16 14:14:51 +0530

    def is_it_ambient(m,n):     sum=0     for i in range(1,m):         if m%i==0:             sum=sum+i     if sum==n:         return  True     else:         return False m=int(input()) n=int(input()) if(is_it_ambient(m,n)):     m,n=n,m     if (is_it_ambient(m,n)):         print('Amicable')     else:         print('Not Amicable') else:     print('Not Amicable')

---
+++ Amicable_Numbers.py
@@ -0,0 +1,19 @@
+def is_it_ambient(m,n):
+    sum=0
+    for i in range(1,m):
+        if m%i==0:
+            sum=sum+i
+    if sum==n:
+        return  True
+    else:
+        return False
+m=int(input())
+n=int(input())
+if(is_it_ambient(m,n)):
+    m,n=n,m
+    if (is_it_ambient(m,n)):
+        print('Amicable')
+    else:
+        print('Not Amicable')
+else:
+    print('Not Amicable')
