4b720a0783988ebbc0428e8e9e1999a852cb3d99
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-05-22 10:38:28 +0530

    n=int(input()) for i in range(n,0,-1):     for j in range(n,0,-1):         if(i==j):             print(i,end=' ')         elif(j==n-i+1):             print(i,end=' ')         else:             print("",end=' ')     print()

---
+++ Print_the_following_pattern.py
@@ -0,0 +1,10 @@
+n=int(input())
+for i in range(n,0,-1):
+    for j in range(n,0,-1):
+        if(i==j):
+            print(i,end=' ')
+        elif(j==n-i+1):
+            print(i,end=' ')
+        else:
+            print("",end=' ')
+    print()
