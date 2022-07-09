23f755f1fa1e13f3124aec8bbe8ae96995ae4c59
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-07-09 10:37:47 +0530

    n=int(input()) for i in range(n):     s=input()     f=0     for i in s:         if i.isdigit():             f=1         else:             f=0             break     if f==1:         print(True)     else:         print(False)

---
+++ String_decimal_.py
@@ -0,0 +1,14 @@
+n=int(input())
+for i in range(n):
+    s=input()
+    f=0
+    for i in s:
+        if i.isdigit():
+            f=1
+        else:
+            f=0
+            break
+    if f==1:
+        print(True)
+    else:
+        print(False)
