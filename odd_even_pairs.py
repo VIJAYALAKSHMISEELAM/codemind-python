944e2b59d49c232d73152ec586e84b77d90e3733
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-08-28 19:07:44 +0530

    n=int(input()) l=list(map(int,input().split())) ev=[] od=[] k=[] for i in range(len(l)):     if(l[i]%2==0):         ev.append(l[i])     else:         od.append(l[i]) if(len(ev)>len(od)):     for i in range(len(od)):         k.append(od[i])         k.append(ev[i])     for i in range(len(od),len(ev)):         k.append(ev[i])     if(n%2==0):         print(*k)     else:         k.append(0)         print(*k) elif(len(ev)<len(od)):
        for i in range(len(ev)):
            
            k.append(od[i])
            k.append(ev[i])
        for i in range(len(ev),len(od)):
            k.append(od[i])
        if(n%2==0):
            print(*k)
        else:
            k.append(0)
            print(*k)
    else:
        for i in range(len(od)):
            k.append(od[i])
            k.append(ev[i])
        print(*k)

---
+++ odd_even_pairs.py
@@ -0,0 +1,40 @@
+n=int(input())
+l=list(map(int,input().split()))
+ev=[]
+od=[]
+k=[]
+for i in range(len(l)):
+    if(l[i]%2==0):
+        ev.append(l[i])
+    else:
+        od.append(l[i])
+if(len(ev)>len(od)):
+    for i in range(len(od)):
+        k.append(od[i])
+        k.append(ev[i])
+    for i in range(len(od),len(ev)):
+        k.append(ev[i])
+    if(n%2==0):
+        print(*k)
+    else:
+        k.append(0)
+        print(*k)
+elif(len(ev)<len(od)):
+    
+    for i in range(len(ev)):
+        
+        k.append(od[i])
+        k.append(ev[i])
+    for i in range(len(ev),len(od)):
+        k.append(od[i])
+    if(n%2==0):
+        print(*k)
+    else:
+        k.append(0)
+        print(*k)
+else:
+    for i in range(len(od)):
+        k.append(od[i])
+        k.append(ev[i])
+    print(*k)
+        
