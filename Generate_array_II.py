ed7ed434275128ec1aa62779d2049d5e53ee7783
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-08-28 19:33:55 +0530

    n=int(input()) l=list(map(int,input().split())) a=[] rep=[] for i in range(n):     if(i%2==0):         for j in range(l[i+1]):             print(l[i],end=' ')

---
+++ Generate_array_II.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int,input().split()))
+a=[]
+rep=[]
+for i in range(n):
+    if(i%2==0):
+        for j in range(l[i+1]):
+            print(l[i],end=' ')
+            
+        
+
+        
