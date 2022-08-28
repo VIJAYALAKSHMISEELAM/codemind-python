aaa6173c1dc03ef96b2e7afef9a469458c8ad9a6
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-08-28 22:33:30 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() l=[] for i in s1:     if i in s2:         if i not in l:             if(i==' '):                 continue             else:                 l.append(i) k=sorted(l) if len(k)==0:     print("-1") else:     for i in k:         print(i,end='')

---
+++ common_characters.py
@@ -0,0 +1,18 @@
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+l=[]
+for i in s1:
+    if i in s2:
+        if i not in l:
+            if(i==' '):
+                continue
+            else:
+                l.append(i)
+k=sorted(l)
+if len(k)==0:
+    print("-1")
+else:
+    for i in k:
+        print(i,end='')
