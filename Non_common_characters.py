aadc5ca044fe8a516fe917681c5ff0c53cd420cc
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-11-26 22:43:45 +0530

    s1=input() s1=s1.lower() s2=input() s2=s2.lower() k=0 s=[] for i in s2:     if i not in s1:         if i==" ":             continue         if i not in s:             s.append(i) for i in s1:     if i not in s2:         if i==" ":             continue         if i not in s:             s.append(i) print(len(s))

--- Non_common_characters.py
+++ Non_common_characters.py
@@ -1,23 +1,19 @@
 s1=input()
-s2=input()
 s1=s1.lower()
+s2=input()
 s2=s2.lower()
-l=[]
-for i in s1:
-    if i not in s2:
-        if i not in l:
-            if(i==' '):
-                continue
-            else:
-                l.append(i)
+k=0
+s=[]
 for i in s2:
     if i not in s1:
-        if i not in l:
-            if(i==' '):
-                continue
-            else:
-                l.append(i)
-if(len(l)==0):
-    print('-1')
-else:
-    print(len(l))
+        if i==" ":
+            continue
+        if i not in s:
+            s.append(i)
+for i in s1:
+    if i not in s2:
+        if i==" ":
+            continue
+        if i not in s:
+            s.append(i)
+print(len(s))
